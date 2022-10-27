5bb7336c8657dd25631a89cb78ecd261d9a17f01
Ola Kalisz <Busiu@Mariuss-MacBook-Pro.local>
2022-10-27 14:46:06 +0100

    Test jupytext notebooks

---
+++ playground.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.0
+#   kernelspec:
+#     display_name: Python [conda env:SE_course]
+#     language: python
+#     name: conda-env-SE_course-py
+# ---
+
+a = 5
+
+b = 3
+
+a + b
+
+
